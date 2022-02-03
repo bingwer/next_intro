d9db0279a44f70c87387b47431a8d9aa49b701ba
Seunghyo Lee <nick.lee.5002@ecoflag.co.kr>
2022-02-03 18:33:39 +0900

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      {/* 글로벌 속성을 추가하고싶을땐 아래와 같이*/}
+      <style global jsx>{`
+        body {
+          background-color: lightgray;
+        }
+      `}</style>
+    </Layout>
+  );
+}
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Seo from '../components/Seo';
+
+function About() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+    </div>
+  );
+}
+
+export default About;
